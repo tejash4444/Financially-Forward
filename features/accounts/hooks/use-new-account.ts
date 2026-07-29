479a37b1119e41e5ad92224fe62a3133cddca749
Tejash Poddar <tejashpoddarvis@gmail.com>
2026-07-29 15:37:33 +0530

    refactor: restructure component hierarchy

--- features/accounts/hooks/use-new-account.ts
+++ features/accounts/hooks/use-new-account.ts
@@ -11,4 +11,4 @@ export const useNewAccount = create <NewAccountState>((set) => ({
     isOpen: false,
     onOpen: () => set({ isOpen:true}),
     onClose: () => set({isOpen:false}),
-}))  
+}))   
