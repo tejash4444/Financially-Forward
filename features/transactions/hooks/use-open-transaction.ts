ccb880bc223ea904a3e0c09821c7738cb0ef40ec
Tejash Poddar <tejashpoddarvis@gmail.com>
2026-08-07 23:07:23 +0530

    chore: configure ESLint rules

--- features/transactions/hooks/use-open-transaction.ts
+++ features/transactions/hooks/use-open-transaction.ts
@@ -13,4 +13,4 @@ export const useOpenTransaction = create <OpenTransactionState>((set) => ({
     isOpen: false,
     onOpen: (id:string) => set({ isOpen:true ,id}),
     onClose: () => set({isOpen:false,id:undefined}),
-}))  
+}))   
